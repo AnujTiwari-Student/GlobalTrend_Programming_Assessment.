98e1172ddcfdf35c4ebe4863c91eeeb77ebdbcf8
Anuj Kumar Tiwari <anujavengers@gmail.com>
2024-07-16 11:23:00 +0530

    Create filterOut.js

---
+++ filterOut.js
@@ -0,0 +1,9 @@
+
+function filterOut(numbers) {
+  return numbers.filter(num => num % 2 !== 0);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const oddNumbers = filterOut(numbers);
+
+console.log(oddNumbers);
